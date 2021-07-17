a24a500aea66956f1ac9306644d7fe62abe77779
Wunder Dmitry <github@testchamber.one>
2021-07-17 15:17:59 +0300

    Now bot is able to do Instagram

--- README.md
+++ README.md
@@ -2,15 +2,28 @@
 
 [@rss_my_ass_bot](http://t.me/rss_my_ass_bot)
 
-This Telegram bot is able to generate RSS feeds for YouTube channels and playlists. Send `/help` to see what it can do.
+This Telegram bot is able to generate RSS feeds for YouTube and beyond (with the power of [rss-bridge](https://github.com/RSS-Bridge/rss-bridge)). Send `/help` to see what it can do.
 
 ## Self hosting
 
-Of course you can selfhost it. Just [create new Telegram bot](https://core.telegram.org/bots) and use provided token as `TOKEN` environment variable upon startup. After that you can simply run it however you like, I'll provide [Dockerfile](Dockerfile) just in case. Or you can use prebuilt image from [DockerHub](https://hub.docker.com/r/wunderwaffla/rss-my-ass)
+Of course you can selfhost it. There are 2 prerequisites:
 
-```
-docker run -e TOKEN=your_token wunderwaffla/rss-my-ass
-```
+1. [Create new Telegram bot](https://core.telegram.org/bots)
+2. Spin up [rss-bridge](https://github.com/RSS-Bridge/rss-bridge) instance if you want to generate feeds not only for YouTube
+3. Rename `.env.example` to `.env`  
+`cp .env.example .env`
+4. Insert Telegram bot token and rss-bridge instance host in `.env` file
+
+**If you want to run it locally**
+
+- `pip install -r requirements.txt`
+- `python bot.py`
+
+**If you want to run it in docker**
+
+- `docker run --env-file .env wunderwaffla/rss-my-ass`
+
+I'll provide [Dockerfile](Dockerfile) just in case or you can use prebuilt image from [DockerHub](https://hub.docker.com/r/wunderwaffla/rss-my-ass)
 
 ---
 
@@ -21,3 +34,5 @@ You can help me to pay electric bill if you're not feeling like hosting it yours
 ---
 
 ![logo](logo.png)
+
+[Art is not mine](https://www.deviantart.com/syrupyyy/art/Lil-Horse-874065692), I just slapped RSS logo where it belonds
--- bot.py
+++ bot.py
@@ -1,20 +1,52 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from os import environ
-from urllib.parse import urlparse, parse_qs
+from dotenv import load_dotenv
+from logging import basicConfig, getLogger, INFO
+from configparser import ConfigParser
+from requests import get
+from urllib.parse import urlparse, parse_qs, urlunparse, urlencode
 from telegram import Update
 from bs4 import BeautifulSoup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-from logging import basicConfig, getLogger, INFO
-from requests import get
-
+load_dotenv()
+TOKEN = environ.get('TOKEN')
+RSS_BRIDGE = environ.get('RSS_BRIDGE')
 
 basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=INFO
 )
 
 logger = getLogger(__name__)
+
+
+def is_url_valid(url: str) -> bool:
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except:
+        return False
+
+
+def sanitize_markdown_string(text: str) -> str:
+    return text.replace('-', '\-').replace('.', '\.').replace('!', '\!')
+
+
+def build_rss_bridge_feed(bridge: str, arguments: dict) -> str:
+    required_query = dict(
+        action='display', media_type='all', format='Atom')
+    required_query['bridge'] = bridge
+    query = required_query | arguments
+    setup = (
+        'https',  # scheme
+        f"{RSS_BRIDGE}",  # netloc
+        '/',  # path
+        '',  # params
+        f"{urlencode(query)}",  # query
+        ''  # fragment
+    )
+    return urlunparse(setup)
 
 
 class FeedSource(ABC):
@@ -25,16 +57,50 @@ class FeedSource(ABC):
     @staticmethod
     @abstractmethod
     def validate_url(url: str, update: Update) -> bool:
-        if YouTube.validate_url(url=url):
-            return "YouTube"
+        if is_url_valid(url):
+            if YouTube.validate_url(url=url):
+                return "YouTube"
+            if Instagram.validate_url(url=url):
+                return "Instagram"
+            else:
+                update.message.reply_text(
+                    'Looks like I do not support such link')
+                logger.warn(f'Could not parse link {url}')
         else:
-            update.message.reply_text('Looks like I do not support such link')
-            logger.warn(f'Could not parse link {url}')
-        pass
+            update.message.reply_text('Looks like this is not a URL')
+            logger.warn(f'Not a URL {url}')
 
     @abstractmethod
     def generate_rss(self, url: str) -> str:
         pass
+
+
+class Instagram(FeedSource):
+
+    def __init__(self, url: str):
+        if "explore/tags" in url:
+            self.id_type = "Hashtag"
+            self.short_type = "h"
+        else:
+            self.id_type = "Username"
+            self.short_type = "u"
+
+    @staticmethod
+    def validate_url(url: str):
+        if "instagram.com" not in url:
+            return False
+        return True
+
+    def generate_rss(self, url: str) -> str:
+        content_id = urlparse(url).path.split("/")[-2]
+        logger.info(f"Instagram {self.id_type} '{content_id}'")
+        arguments = {
+            'context': self.id_type,
+            self.short_type: content_id
+        }
+        feed_url = build_rss_bridge_feed(
+            bridge="Instagram", arguments=arguments)
+        return f"Enjoy your Instagran {self.id_type} feed\n{feed_url}"
 
 
 class YouTube(FeedSource):
@@ -71,32 +137,42 @@ class YouTube(FeedSource):
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text(
-        'Send me a link to YouTube channel and I\'ll generate RSS feed of the channel.\nI can also generate channel RSS from any video from that channel.\nAnd I can generate YouTube playlist RSS feed if you send me playlist URL')
+    update.message.reply_text(sanitize_markdown_string(
+        """I can generate RSS feeds with the help of [rss-bridge](https://github.com/RSS-Bridge/rss-bridge)
+Now I'm able to generate feeds for Youtube and Instagram
+*I am not a feed reader!* I only generate RSS feed that you can use with another Telegram bot or your reader of choice
+Thanks for using. You can see bot source code on [github](https://github.com/wunderwaffla/rss-my-ass)"""), parse_mode='MarkdownV2')
 
 
 def create_rss(update: Update, context: CallbackContext) -> None:
     url = update.message.text
     feed_type = FeedSource.validate_url(url=url, update=update)
-    if feed_type == "YouTube":
-        feed = YouTube(url=url)
+    if feed_type:
+        if feed_type == "Instagram":
+            feed = Instagram(url=url)
+        elif feed_type == "YouTube":
+            feed = YouTube(url=url)
         link = feed.generate_rss(url=url)
         update.message.reply_text(link)
 
 
 def main() -> None:
-    TOKEN = environ.get('TOKEN')
-    if environ.get('TOKEN') is None:
+
+    if TOKEN is None:
         logger.error(
             'Please specify TOKEN environment variable with Telegram bot token')
         exit(1)
+    if RSS_BRIDGE is None:
+        logger.error(
+            'Please specify RSS_BRIDGE environment variable with rss-bridge hostname')
+        exit(1)
 
-    updater = Updater(TOKEN)
+    updater = Updater(environ.get('TOKEN'))
 
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("start", help_command))
 
     dispatcher.add_handler(MessageHandler(
         Filters.text & ~Filters.command, create_rss))
